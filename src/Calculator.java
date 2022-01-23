31315c0c6351cf229c39644f9a6d18775a723eea
Korobova Irina <korbovartem@gmail.com>
2022-01-23 22:18:53 +0500

    First commit

---
+++ src/Calculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        char calculate = 'a';
+        do {
+        double operand1 = scanner.nextInt();
+        char operation = scanner.next().charAt(0);
+        double operand2 = scanner.nextInt();
+        System.out.println(calcul(operand1, operation, operand2));
+        } while (calculate != 'c');
+    }
+
+    public static double calcul(double operand1, char operation, double operand2) {
+        switch (operation) {
+            case '+':
+                return operand1 + operand2;
+            case '-':
+                return operand1 - operand2;
+            case '*':
+                return operand1 * operand2;
+            case '/':
+                return operand1 / operand2;
+            default:
+                System.out.println("error");
+                return 0;
+            }
+
+        };
+
+    }
+
+
